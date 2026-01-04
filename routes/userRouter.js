29a3eab522bf5259aced8d76a9e157b00122e819
gadji <gadji180105@gmail.com>
2026-01-04 13:53:23 +0300

    feat(controller): добавил контроллер

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,9 +1,10 @@
 const Router = require('express');
+const userController = require("../controller/userController");
 const router = new Router();
 
 
-router.get('/auth', (req, res) => {
-    res.json({message: "user auth"});
-});
+router.get('/auth', userController.check);
+router.post('/register', userController.register);
+router.post('/login', userController.login);
 
 module.exports = router;
