4bf2ddf9ce2c4144cabf9dec6ba13b100d45aedb
gadji <gadji180105@gmail.com>
2026-01-06 16:00:53 +0300

    finish

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ require('dotenv').config()
 
 const express = require('express');
 const sequelize = require('./db');
-const models = require('./models/models');
+const models = require('./src/models/models');
 const cors = require('cors');
-const router = require('./routes/index');
+const router = require('./src/routes/index');
 const PORT = process.env.PORT || 5000;
+const errorHandler = require('./src/middleware/ErrorHandlingMiddleware')
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
-
+app.use(errorHandler);
 
 const start = async () => {
     try {
---
+++ src/controller/userController.js
@@ -0,0 +1,64 @@
+const ApiError = require('../error/ApiError');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { User } = require('../models/models');
+
+const generateJwt = (id, email) => {
+    return jwt.sign(
+        {id, email},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
+class UserController {
+    async register(req,res, next){
+        const { email, password } = req.body;
+        if(!email || !password){
+            return next(ApiError.badRequest('Не верный логин или пароль'))
+        }
+
+        const candidate = await User.findOne({where: {email}});
+        if(candidate) {
+            return next(ApiError.badRequest(''))
+        }
+
+        const hashPassword = await bcrypt.hash(password, 5);
+        const user = await User.create({email, password: hashPassword});
+        const token = generateJwt(user.id, user.email);
+
+        return res.json({token})
+    }
+
+    async login(req,res){
+        const { email, password, next } = req.body;
+
+        const user = await User.findOne({where: {email}});
+
+        if(!user) {
+            return next(ApiError.internal('Пользователь не найден'))
+        }
+
+        let comparePassword = bcrypt.compareSync(password, user.password);
+
+        if(!comparePassword) {
+            return next(ApiError.internal('Указан неверный пароль'));
+        }
+
+        const token = generateJwt(user.id, user.email);
+
+        return res.json({token})
+    }
+
+    async check(req, res, next){
+        const { id } = req.query;
+
+        if(!id) {
+            return next(ApiError.badRequest("Не задан ID"));
+        }
+
+        res.json(id);
+    }
+}
+
+module.exports = new UserController();
